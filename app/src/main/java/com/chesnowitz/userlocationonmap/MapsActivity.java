26f54a3d6b8e76b47b4349c42f2b5aab313bc915
Stephen Chesnowitz <steve@chesnowitz.com>
2017-05-10 13:34:32 -0400

    getting info but not working

--- app/src/main/java/com/chesnowitz/userlocationonmap/MapsActivity.java
+++ app/src/main/java/com/chesnowitz/userlocationonmap/MapsActivity.java
@@ -3,6 +3,8 @@ package com.chesnowitz.userlocationonmap;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,9 +25,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
     LocationManager locationManager;
     LocationListener locationListener;
 
@@ -75,10 +83,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                Toast.makeText(MapsActivity.this, location.
 
             LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.clear();
+
+            mMap.clear();
+
             mMap.addMarker(new MarkerOptions().position(userLocation).title("My Location"));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
 
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listOfAddresses = geocoder.getFromLocation
+                            (location.getLatitude(), location.getLongitude(), 1);
+                    Log.i("Location", listOfAddresses.get(1).toString());
+                    if (listOfAddresses != null && listOfAddresses.size() > 0) {
+                        Log.i("Location", listOfAddresses.get(0).getPostalCode());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
